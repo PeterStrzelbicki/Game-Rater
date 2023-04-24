fcb6d14291aa5d226551806be3af7039b6965bee
PeterStrzelbicki <strzelbicki.peter@gmail.com>
2023-04-24 19:11:24 -0400

    Created a general structure for the application.

---
+++ Game Rater/Game Rater/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿/*
+* FILE          :   MainWindow.xaml.cs
+* PROJECT       :   Game Rater
+* PROGRAMMER    :   Peter Strzelbicki
+* FIRST VERSION :   April 24th, 2023
+* DESCRIPTION   :   This files contains the code for the main application display.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Game_Rater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void searchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void searchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (searchBox.Text == "Search")
+            {
+                searchBox.Text = "";
+                searchBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void searchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(searchBox.Text))
+            {
+                searchBox.Text = "Search";
+                searchBox.Foreground = Brushes.Gray;
+            }
+        }
+    }
+}
