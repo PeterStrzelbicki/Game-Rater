16db98580ee509aa9ddeaa6337d0a2d3d4bb3bfc
PeterStrzelbicki <strzelbicki.peter@gmail.com>
2023-04-25 19:45:28 -0400

    Search functionality has been implemented.

--- Game Rater/Game Rater/MainWindow.xaml.cs
+++ Game Rater/Game Rater/MainWindow.xaml.cs
@@ -3,7 +3,7 @@
 * PROJECT       :   Game Rater
 * PROGRAMMER    :   Peter Strzelbicki
 * FIRST VERSION :   April 24th, 2023
-* DESCRIPTION   :   This files contains the code for the main application display.
+* DESCRIPTION   :   This files contains the code for the main application.
 */
 using System;
 using System.Collections.Generic;
@@ -19,23 +19,58 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Net.Http.Headers;
 
 namespace Game_Rater
-{
+{        
+    public class Game
+        {
+            public string Name { get; set; }
+            public int Score { get; set; }
+        }
+
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public ObservableCollection<Game> Games { get; set; } = new ObservableCollection<Game>();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            searchBox.Foreground = Brushes.Gray;
+            
+            Games.Add(new Game {Name = "Game 1", Score = 10});
+            Games.Add(new Game { Name = "Game 2", Score = 5});
+            Games.Add(new Game { Name = "Persona 5", Score = 11 });
+            Games.Add(new Game { Name = "Call of Duty 4", Score = 8 });
+            gameList.ItemsSource = Games;
         }
 
         private void searchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            string searchText = searchBox.Text.Trim();
 
+            if (gameList != null)
+            {
+                if (searchText != "Search" || !string.IsNullOrEmpty(searchText))
+                {
+                    var viewSource = CollectionViewSource.GetDefaultView(gameList.ItemsSource);
+                    viewSource.Filter = g => ((Game)g).Name.IndexOf(searchText, StringComparison.OrdinalIgnoreCase) >= 0;
+                }
+                else
+                {
+                    var viewSource = CollectionViewSource.GetDefaultView(gameList.ItemsSource);
+                    viewSource.Filter = null;
+                }
+            }
         }
+
         private void searchBox_GotFocus(object sender, RoutedEventArgs e)
         {
             if (searchBox.Text == "Search")
@@ -62,6 +97,11 @@ namespace Game_Rater
         {
             AboutWindow aboutWindow = new AboutWindow();
             aboutWindow.ShowDialog();
+        }
+
+        private void newGameBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
